b5ea8390fb802ebd063a3e77ff01ede7b92a839d
honglamvan <81795183+lamvanhongdev@users.noreply.github.com>
2023-02-17 09:45:05 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+require("dotenv/config");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// user authentication routes
+const userRoute = require("./routes/auth");
+app.use("/api/users/", userRoute);
+
+// Artist links
+const artistsRoute = require("./routes/artists");
+app.use("/api/artists/", artistsRoute);
+
+// Album links
+const albumRoute = require("./routes/albums");
+app.use("/api/albums/", albumRoute);
+
+// Songs links
+const songRoute = require("./routes/songs");
+app.use("/api/songs/", songRoute);
+
+// If any depreciation warning add depreciation options
+// mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true }, () => {
+//   console.log("Mongodb Connected");
+// });
+
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.hhebfwz.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+mongoose.connection
+  .once("open", () => console.log("Connected"))
+  .on("error", (error) => {
+    console.log(`Error : ${error}`);
+  });
+
+app.listen(443, () => console.log("lisitening to port 4000"));
---
+++ models/word.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const WordSchema = mongoose.Schema(
+  {
+    originWord: {
+      type: String,
+      required: true,
+    },
+    translatedWord: {
+      type: String,
+      required: true,
+    },
+    ipa: {
+      type: String,
+      required: true,
+    },
+    sound: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      default: "en-vi",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("word", WordSchema);
